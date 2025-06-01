9450d9e8cdd75fd624139e39b5f6fd71cb101ebc
prashant <prashantsharma7470@gmail.com>
2025-06-01 16:47:55 +0530

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+    const location = useLocation();
     return (
         < div className="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color" >
             <div className="mobile-sidebar-header d-md-none">
@@ -13,19 +14,19 @@ const Sidebar = () => {
             </div>
             <div className="sidebar-menu-content">
                 <ul className="nav nav-sidebar-menu sidebar-toggle-view">
-                    <li className="nav-item active">
+                    <li className={`nav-item ${location.pathname === "/" ? "active" : ""}`}>
                         <Link to="/" className="nav-link">
                             <i className="flaticon-menu-1" />
                             <span>Dashboard</span>
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className={`nav-item ${location.pathname === "/users-list" ? "active" : ""}`}>
                         <Link to="/users-list" className="nav-link">
                             <i className="fa fa-users" />
                             <span>Users</span>
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className={`nav-item ${location.pathname === "/help-support" ? "active" : ""}`}>
                         <Link to="/help-support" className="nav-link">
                             <i className="fa fa-headphones" />
                             <span>Help &amp; Support</span>
@@ -34,7 +35,6 @@ const Sidebar = () => {
                 </ul>
             </div>
         </div >
-        // {/* Sidebar Area End Here */ }
     )
 }
 
